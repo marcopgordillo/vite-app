a4b278058170847810aed35ecc8dfefdbca3b99b
Marco Gordillo <6589245+marcopgordillo@users.noreply.github.com>
2020-08-13 11:21:52 -0400

    Change message

--- App.vue
+++ App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite + github Actions" />
 </template>
 
 <script>
